303ff3b58fc8d529f41c50ce2cda279be6f43112
Jade Meskill and Roy van de Water <sqrt+jade+roy@octoblu.com>
2015-02-10 15:00:06 -0700

    Add delete old indexes script

---
+++ delete_old_indexes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+EVENTS="100 101 102 200 201 204 205 300 301 302 303 304 305 400 401 402 403 500 600 700 701"
+VERSION="1"
+
+for event in $EVENTS; do
+  # magic
+  curl -XDELETE "http://localhost:9200/meshblu_events_${event}_v${VERSION}"
+done
