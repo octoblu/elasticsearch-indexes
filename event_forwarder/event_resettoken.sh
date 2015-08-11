8b2cac8a1bcad3b52ceca71257726409148991bf
sqrt <sqrt@octoblu.com>
2015-08-11 16:07:55 -0700

    Add index for event_subscribe

---
+++ event_forwarder/event_resettoken.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+EVENT='event_resettoken'
+NEW_VERSION=1
+
+curl -XPUT "http://localhost:9200/${EVENT}_v${NEW_VERSION}" -d '{
+  "mappings": {
+    "event": {
+      "dynamic": false,
+      "_timestamp": {
+        "enabled": true
+      },
+      "properties": {
+        "fromUuid" : {
+          "type": "string",
+          "fields": {
+            "raw" : {
+              "type": "string",
+              "index": "not_analyzed"
+            }
+          }
+        }
+      }
+    }
+  }
+}'
+
+read -r -d '' alias_command <<EOF
+  {
+    "actions": [
+      {
+        "add": {
+            "alias": "${EVENT}",
+            "index": "${EVENT}_v${NEW_VERSION}"
+        }
+      }
+    ]
+  }
+EOF
+curl -XPOST http://localhost:9200/_aliases -d "$alias_command"
