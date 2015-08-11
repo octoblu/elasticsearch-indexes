800526a0274019305479a34fc0db3a137857911b
sqrt <sqrt@octoblu.com>
2015-08-11 15:43:26 -0700

    Add index for event_identity

---
+++ event_forwarder/event_identity.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+EVENT='event_devices'
+OLD_VERSION=$1
+NEW_VERSION=$2
+
+curl -XPUT "http://localhost:9200/${EVENT}_v${NEW_VERSION}" -d '{
+  "mappings": {
+    "event": {
+      "dynamic": false,
+      "_timestamp": {
+        "enabled": true
+      },
+      "properties": {
+      }
+    }
+  }
+}'
+
+read -r -d '' alias_command <<EOF
+  {
+    "actions": [
+      {
+        "add": {
+            "alias": "${EVENT}",
+            "index": "${EVENT}_v${NEW_VERSION}"
+        }
+      }
+    ]
+  }
+EOF
+curl -XPOST http://localhost:9200/_aliases -d "$alias_command"
