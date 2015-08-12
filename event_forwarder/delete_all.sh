3910168c1228fa588813172148c8807f8878302b
sqrt <sqrt@octoblu.com>
2015-08-12 08:41:49 -0700

    Add missing types

--- event_forwarder/delete_all.sh
+++ event_forwarder/delete_all.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-EVENTS=(event_claimdevice event_data event_devices event_generatetoken event_getpublickey event_identity event_localdevices event_message event_register event_resettoken event_revoketoken event_subscribe event_unclaimeddevices event_unregister event_update event_whoami)
+EVENTS=(event_claimdevice event_data event_devices event_generatetoken event_getpublickey event_identity event_localdevices event_message event_register event_resettoken event_revoketoken event_subscribe event_unclaimeddevices event_undefined event_unregister event_unsubscribe event_update event_whoami)
+
+if [ -n "$1" ]; then
+  EVENTS=($1)
+fi
 
 for EVENT in "${EVENTS[@]}"; do
   read -r -d '' alias_command <<EOF
@@ -18,4 +22,5 @@ EOF
 
   curl -X POST http://localhost:9200/_aliases -d "$alias_command"
   curl -X DELETE "http://localhost:9200/${EVENT}_v1"
+  curl -X DELETE "http://localhost:9200/${EVENT}"
 done
---
+++ event_forwarder/event_undefined.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+EVENT='event_undefined'
+NEW_VERSION=1
+
+curl -XPUT "http://localhost:9200/${EVENT}_v${NEW_VERSION}" -d '{
+  "mappings": {
+    "event": {
+      "dynamic": false,
+      "_timestamp": {
+        "enabled": true
+      },
+      "properties": {
+      }
+    }
+  }
+}'
+
+read -r -d '' alias_command <<EOF
+  {
+    "actions": [
+      {
+        "add": {
+            "alias": "${EVENT}",
+            "index": "${EVENT}_v${NEW_VERSION}"
+        }
+      }
+    ]
+  }
+EOF
+curl -XPOST http://localhost:9200/_aliases -d "$alias_command"
