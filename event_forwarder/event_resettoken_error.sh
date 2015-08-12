2bdf577e977b3fc8cfcfe3b974f7e03f9ac47034
sqrt <sqrt@octoblu.com>
2015-08-12 08:54:20 -0700

    Add error types

---
+++ event_forwarder/event_resettoken_error.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+EVENT='event_resettoken_error'
+NEW_VERSION=1
+
+curl -XPUT "http://localhost:9200/${EVENT}_v${NEW_VERSION}" -d '{
+  "mappings": {
+    "event": {
+      "dynamic": false,
+      "_timestamp": {
+        "enabled": true
+      },
+      "properties": {
+        "fromUuid" : {
+          "type": "string",
+          "fields": {
+            "raw" : {
+              "type": "string",
+              "index": "not_analyzed"
+            }
+          }
+        },
+        "error" : {
+          "type": "string",
+          "fields": {
+            "raw" : {
+              "type": "string",
+              "index": "not_analyzed"
+            }
+          }
+        }
+      }
+    }
+  }
+}'
+
+read -r -d '' alias_command <<EOF
+  {
+    "actions": [
+      {
+        "add": {
+            "alias": "${EVENT}",
+            "index": "${EVENT}_v${NEW_VERSION}"
+        }
+      }
+    ]
+  }
+EOF
+curl -XPOST http://localhost:9200/_aliases -d "$alias_command"
