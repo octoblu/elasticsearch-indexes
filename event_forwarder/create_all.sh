17c95c4a5340598fba5e1d62c1e8b8cd9f916073
sqrt <sqrt@octoblu.com>
2015-08-12 09:11:42 -0700

    create_all probably works better when not commented out...

--- event_forwarder/create_all.sh
+++ event_forwarder/create_all.sh
@@ -4,6 +4,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 FILES=($(ls $DIR/event_*.sh))
 
 for FILE in "${FILES[@]}"; do
-  echo $FILE
-  # bash "${EVENT}.sh"
+  bash "${EVENT}.sh"
 done
