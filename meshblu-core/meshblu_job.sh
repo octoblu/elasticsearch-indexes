815739f1a99f3923a9b9ccfafd5fc504ad7f834f
sqrt <sqrt@octoblu.com>
2016-01-14 11:44:27 -0700

    Made meshblu_job a template

--- meshblu-core/meshblu_job.sh
+++ meshblu-core/meshblu_job.sh
@@ -1,11 +1,115 @@
 #!/bin/bash
 
 ES_HOST='https://search-meshlastic-jzohajyndq6bowz24ic2jnf3vu.us-west-2.es.amazonaws.com'
-INDEX='meshblu_job'
-NEW_VERSION=1
 
-curl -XPUT "${ES_HOST}/${INDEX}_v${NEW_VERSION}" -d '{
+curl -XPUT "${ES_HOST}/_template/meshblu_job" -d '{
+  "template": "meshblu_job*",
+  "order": 1,
+  "aliases": {
+    "meshblu_job_last_7_days": {}
+  },
   "mappings": {
+    "job": {
+      "dynamic": false,
+      "_timestamp": {
+        "enabled": true,
+        "store": true
+      },
+      "properties": {
+         "date": {
+           "type": "date"
+         },
+         "elapsedTime": {
+            "type": "long"
+         },
+         "request": {
+            "properties": {
+               "metadata": {
+                  "properties": {
+                     "auth": {
+                        "properties": {
+                           "uuid": {
+                              "type": "string",
+                              "fields": {
+                                "raw" : {
+                                  "type": "string",
+                                  "index": "not_analyzed"
+                                }
+                              }
+                           }
+                        }
+                     },
+                     "fromUuid": {
+                        "type": "string",
+                        "fields": {
+                          "raw" : {
+                            "type": "string",
+                            "index": "not_analyzed"
+                          }
+                        }
+                     },
+                     "jobType": {
+                        "type": "string",
+                        "fields": {
+                          "raw" : {
+                            "type": "string",
+                            "index": "not_analyzed"
+                          }
+                        }
+                     },
+                     "responseId": {
+                        "type": "string",
+                        "fields": {
+                          "raw" : {
+                            "type": "string",
+                            "index": "not_analyzed"
+                          }
+                        }
+                     },
+                     "toUuid": {
+                        "type": "string",
+                        "fields": {
+                          "raw" : {
+                            "type": "string",
+                            "index": "not_analyzed"
+                          }
+                        }
+                     }
+                  }
+               }
+            }
+         },
+         "response": {
+            "properties": {
+               "metadata": {
+                  "properties": {
+                     "code": {
+                        "type": "long"
+                     },
+                     "responseId": {
+                        "type": "string",
+                        "fields": {
+                          "raw" : {
+                            "type": "string",
+                            "index": "not_analyzed"
+                          }
+                        }
+                     },
+                     "status": {
+                        "type": "string",
+                        "fields": {
+                          "raw" : {
+                            "type": "string",
+                            "index": "not_analyzed"
+                          }
+                        }
+                     }
+                  }
+               }
+            }
+         }
+      }
+    },
     "dispatcher": {
       "dynamic": false,
       "_timestamp": {
@@ -109,17 +213,3 @@ curl -XPUT "${ES_HOST}/${INDEX}_v${NEW_VERSION}" -d '{
     }
   }
 }'
-
-read -r -d '' alias_command <<EOF
-  {
-    "actions": [
-      {
-        "add": {
-            "alias": "${INDEX}",
-            "index": "${INDEX}_v${NEW_VERSION}"
-        }
-      }
-    ]
-  }
-EOF
-curl -XPOST "${ES_HOST}/_aliases" -d "$alias_command"
