64bab1e6eecba2af4b39f83acb01ff243263959d
sqrtofsaturn <sqrtofsaturn@gmail.com>
2016-11-18 14:28:52 -0700

    Add genisys values

---
+++ meshlastic/genisys.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+ELASTIC_PORT=${ELASTIC_PORT:-9200}
+BASE_URL=${BASE_URL:-http://localhost:${ELASTIC_PORT}}
+
+curl -XPUT "${BASE_URL}/_template/genisys-room-state" -d '{
+  "template": "genisys-room-state*",
+  "order": 1,
+  "mappings": {
+    "_default_": {
+      "dynamic": false,
+      "properties": {
+        "uuid": {
+          "type": "string",
+          "index": "not_analyzed"
+        },
+        "meshblu": {
+          "type": "object",
+          "properties": {
+            "updatedAt": {
+              "type": "date"
+            },
+            "updatedBy": {
+              "type": "string",
+              "index": "not_analyzed"
+            }
+          }
+        },
+        "genisys": {
+          "type": "object",
+          "properties": {
+            "inSkype": {
+              "type": "boolean"
+            },
+            "currentMeeting": {
+              "type": "object",
+              "properties": {
+                "meetingId": {
+                  "type": "string",
+                  "index": "not_analyzed"
+                },
+                "startTime": {
+                  "type": "date"
+                },
+                "endTime": {
+                  "type": "date"
+                },
+                "subject": {
+                  "type": "string",
+                  "index": "not_analyzed"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}'
